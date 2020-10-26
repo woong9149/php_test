71ae1ef87b9249ad8b5cfcc71f73828572dda2fa
woong9149 <sewoong0409@naver.com>
2020-10-26 23:34:43 +0900

    함수를 활용한 index.php 수정
    함수를 활용한 index.php 수정

--- index.php
+++ index.php
@@ -1,42 +1,55 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-</head>
-<body>
+<?php
+function print_title(){
+  if(isset($_GET['id'])){
+    echo $_GET['id'];
+  } else {
+    echo "Welcome";
+  }
+}
+function print_description(){
+  if(isset($_GET['id'])){
+    echo file_get_contents("data/".$_GET['id']);
+  } else {
+    echo "Hello, PHP";
+  }
+}
+function print_list(){
+  $list = scandir('./data');
+  $i = 0;
+  while($i < count($list)){
+    if($list[$i] != '.') {
+      if($list[$i] != '..') {
+        echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+      }
+    }
+    $i = $i + 1;
+  }
+}
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+      <?php
+      print_title();
+      ?>
+    </title>
+  </head>
+  <body>
     <h1><a href="index.php">WEB</a></h1>
     <ol>
-    <?php
-        $list = scandir('data');
-        $i = 0;
-        while($i < count($list)){
-            if($list[$i] != '.'){
-                ?>       
-                <li><a href="index.php?id=<?=$list[$i]?>"><?=$list[$i]?></a></li>
-                <?php
-            }
-        }
-        $i = $i + 1;
-    }
-    ?>
-        
+      <?php
+      print_list();
+      ?>
     </ol>
     <h2>
-        <?php
-        if(isset($_GET['id'])){
-            echo $_GET['id'];
-        }else{
-            echo 'welcome';
-        }
-            
-        ?>
+      <?php
+      print_title();
+      ?>
     </h2>
-   <?php 
-    if(isset($_GET['id'])){
-        echo file_get_contents("data/".$_GET['id']);
-    }else{
-        echo 'Hello, PHP';
-    }
-    
-   ?>
-</body>
+    <?php
+    print_description();
+     ?>
+  </body>
 </html>
